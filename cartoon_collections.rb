607ed984ebcf0c9724be3fa7e328585cdb5bd0ed
Paul Ralyea <047par@gmail.com>
2020-05-12 20:39:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
     planeteer_calls.map do |element|
-      if planeteer_calls.include? ("Earth!")
+      if planeteer_calls.include? (valid_calls)
         puts "Earth!"
       #else if planeteer_calls.include? ("Wind!")
        # puts "Wind!"
