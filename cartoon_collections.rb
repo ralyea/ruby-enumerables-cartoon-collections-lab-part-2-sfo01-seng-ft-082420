cd1d376bbf332006df1d8cbd86ba01bef29c968b
Paul Ralyea <047par@gmail.com>
2020-05-12 19:38:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   
   planeteer_calls.map do |element|
-    if element.length > 4
-      puts true
-    
+    element.length > 4
   end
 end
 
